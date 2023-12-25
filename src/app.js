1ad49152cbac0e12ac330659c27d3da2c7b50732
Agustin Salvidio <agustinsalvidio@gmail.com>
2023-12-25 13:46:00 -0300

    :sparkles: Added routing

--- src/app.js
+++ src/app.js
@@ -1,43 +1,19 @@
 import express from "express";
-import { ProductManagerFileBased } from "./main/ProductManager/ProductManagerFileBased.js";
+import productRouter from "./routers/products.routers.js";
 
 const app = express();
-const path = "./resources/ProductsMock.json";
-
-const productManager = new ProductManagerFileBased(path);
+const PORT = 8080;
 
 const configureApp = () => {
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
-  app.listen(8080, () => {
-    console.log("Escuchando en el puerto 8080");
+  app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`);
   });
 };
 
 const configureEndpoints = () => {
-  app.get("/products", async (req, res) => {
-    try {
-      const { limit } = req.query;
-      const products = await productManager.getProducts();
-      if (!limit) {
-        return res.status(200).send(products);
-      } else {
-        const filteredProducts = products.slice(0, limit);
-        return res.status(200).send(filteredProducts);
-      }
-    } catch (error) {
-      return res.status(400).send(error.message);
-    }
-  });
-  app.get("/products/:pid", async (req, res) => {
-    try {
-      const { pid } = req.params;
-      const foundProduct = await productManager.getProductById(parseInt(pid));
-      return res.status(200).send(foundProduct);
-    } catch (error) {
-      return res.status(400).send(error.message);
-    }
-  });
+  app.use("/", productRouter);
 };
 
 const initializeApp = () => {
