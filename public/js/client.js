ac633e4a2c745e7352cd865985633c4ec60ad437
Agustin Salvidio <agustinsalvidio@gmail.com>
2023-12-26 16:47:36 -0300

    :lipstick: Added add product modal

--- public/js/client.js
+++ public/js/client.js
@@ -2,11 +2,60 @@ const socket = io();
 
 const registerNewProduct = () => {
   Swal.fire({
-    imageUrl: anImageUrl,
-    imageWidth: 600,
-    imageHeight: 300,
-    imageAlt: "A product image",
-    showConfirmButton: false,
+    title: "Agregar Producto",
+    html: `
+    <div class="modal-body">
+      <div class="container-fluid">
+        <div class="row text-left">
+          <div class="col-md-6">
+            <label for="title" class="col-form-label">Título</label>
+            <input id="title" type="text" name="title" class="form-control" placeholder="Título"> 
+          </div>
+          <div class="col-md-6">
+            <label for="description" class="col-form-label">Descripción</label>
+            <input id="description" type="text" name="description" class="form-control" placeholder="Descripción">
+          </div>
+          <div class="col-md-6">
+            <label for="price" class="col-form-label">Precio</label>
+            <input id="price" type="text" name="price" class="form-control" placeholder="Precio">
+          </div>
+          <div class="col-md-6">
+            <label for="stock" class="col-form-label">Stock</label>
+            <input id="stock" type="text" name="stock" class="form-control" placeholder="Stock">
+          </div>
+          <div class="col-md-6">
+            <label for="code" class="col-form-label">Código:/label>
+            <input id="code" type="text" name="code" class="form-control" placeholder="Código">
+          </div>
+          <div class="col-md-6">
+            <label for="thumbnail" class="col-form-label">Imagen</label>
+            <input id="thumbnail" type="file" class="form-control" placeholder="Seleccione imagen">
+          </div>
+        </div>   
+      </div>
+    </div>
+    `,
+    showCancelButton: true,
+    cancelButtonText: "Cancelar",
+    confirmButtonText: "Agregar",
+    confirmButtonColor: "#b61212",
+    preConfirm: () => {
+      const title = document.getElementById("title").value;
+      const description = document.getElementById("description").value;
+      const price = document.getElementById("price").value;
+      const stock = document.getElementById("stock").value;
+      const code = document.getElementById("code").value;
+      const thumbnail = document.getElementById("thumbnail").files[0];
+
+      console.log("Título:", title);
+      console.log("Descripción:", description);
+      console.log("Precio:", price);
+      console.log("Stock:", stock);
+      console.log("Código:", code);
+      console.log("Imagen:", thumbnail);
+
+      //Hacer magia para guardar la info
+    },
   });
 };
 
